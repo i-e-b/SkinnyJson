786114050f5d7d1c6a4d4ec604b9575fa6b12a27
Iain Ballard <iain.ballard@bjss.com>
2021-05-17 21:06:22 +0100

    net core 5 tests

--- SkinnyJson.NetCore.UnitTests/BasicTests.cs
+++ SkinnyJson.NetCore.UnitTests/BasicTests.cs
@@ -16,14 +16,29 @@ namespace SkinnyJson.NetCore.UnitTests
             Assert.That(result.NullableString, Is.Null, "Nullable type");
             Assert.That(result.NonNullString, Is.Null, "Not null type");
         }
-        
+
+        [Test]
+        public void handling_missing_constructor()
+        {
+            Json.DefaultParameters.EnableAnonymousTypes = true;
+            var result = Json.Defrost<BadConstructor>("{\"StringValue\":\"Hello, World\"}");
+            
+            Assert.That(result, Is.Not.Null, "Outer type");
+            Assert.That(result.StringValue, Is.EqualTo("Hello, World"), "Nullable type");
+        }
+
         public class AnnotatedCoreType
         {
 #pragma warning disable 8618 // warning that non-nullable is not valid
             public string NonNullString { get; set; }
             public string? NullableString { get; set; }
 #pragma warning restore 8618
-        
+        }
+
+        public class BadConstructor
+        {
+            public BadConstructor(int requirement) { }
+            public string? StringValue { get; set; }
         }
     }
 }
